57b01240f063b1d66fae626042da3865e610e454
LucasSMax <lucas.maximiano202@gmail.com>
2022-09-10 15:11:40 -0300

    forecast mapeado como teste funcional espera

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -15,12 +15,18 @@ export interface Beach {
     user: string
 }
 
+export interface TimeForecast {
+    time: string,
+    forecast: BeachForecast[]
+}
+
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint{};
+
 
 export class Forecast{
     constructor(protected stormGlas = new StormGlass()){};
 
-    public async processForecastForBeaches(beaches: Beach[]): Promise<BeachForecast[]> {
+    public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]> {
         const pointWithCorrectSources: BeachForecast[] = [];
         for(const beach of beaches) {
             const points = await this.stormGlas.fetchPoints(beach.lat, beach.lng);
@@ -36,6 +42,22 @@ export class Forecast{
             }));
             pointWithCorrectSources.push(...enrichedBeachData);
         }
-        return pointWithCorrectSources;
+        return this.mapForecastByTime(pointWithCorrectSources);
+    }
+
+    private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+        const forecastByTime: TimeForecast[] = [];
+        for (const point of forecast) {
+            const timePoint = forecastByTime.find((f) => f.time === point.time)
+            if(timePoint) {
+                timePoint.forecast.push(point);
+            } else {
+                forecastByTime.push({
+                    time: point.time,
+                    forecast: [point]
+                })
+            }
+        }
+        return forecastByTime;
     }
 }
